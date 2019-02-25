6ed29051bf1a9f580608506413d32b84327f8815
Dhruv <ddiv739@aucklanduni.ac.nz>
2019-02-26 00:58:28 +1100

    Change text a bit - didnt like the capitalisation

--- pages/projects.js
+++ pages/projects.js
@@ -1,17 +1,17 @@
 import BaseLayout from '../components/layouts/BaseLayout'
-import {MdBuild} from 'react-icons/md'
+import {MdFreeBreakfast} from 'react-icons/md'
 import {Container, Card, Row, Col, CardTitle,CardDeck, CardImg,CardBody,CardText, Button} from 'reactstrap'
 const Projects= () => (
     <BaseLayout>
           <Container className='about-layout index-container'>
 
           <div>
-            <MdBuild style={{  height: '50px', width: '50px',color: 'rgb(183, 58, 73)'}}/>
+            <MdFreeBreakfast style={{  height: '50px', width: '50px',color: 'rgb(183, 58, 73)'}}/>
           </div>
           <h1 className="d-none d-xl-block about-head">Dhruv Divekar</h1>
           <h1 className="d-block d-xl-none about-head">Dhruv</h1>
           <h1 className="d-block d-xl-none about-head">Divekar</h1>
-      <p>A Collection of Projects I consider interesting or a worthwhile learning experience</p>
+      <p>A collection of projects I consider interesting or a worthwhile learning experience</p>
       <hr />
       <CardDeck >
       <Card>
