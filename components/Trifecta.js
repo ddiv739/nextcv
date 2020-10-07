7a3611c89353e469db5436ca5daa9c8c05cb574a
Dhruv Divekar <ddiv739@aucklanduni.ac.nz>
2020-10-07 23:47:28 +1000

    Update Trifecta.js
    may 2

--- components/Trifecta.js
+++ components/Trifecta.js
@@ -23,7 +23,7 @@ const Trifecta = () => (
                                 <div className="row justify-content-center align-items-center">Linkedin</div>
                             </div>
                             <div className="col-sm glyphicon" ><FiCode /></div>
-                            <div className="col-sm hovfadein " onClick={()=>{window.open("https://1drv.ms/b/s!AsTPUiKo9CTXgrpB6ZXTMG3NeVfMXQ?e=RghI1G")}}>
+                            <div className="col-sm hovfadein " onClick={()=>{window.open("https://1drv.ms/b/s!AsTPUiKo9CTXgrpEIvKiqScEToszwA?e=3XJ3Dk")}}>
                                 <div className="row justify-content-center align-items-center"><GoRepo className="glyphicon"/></div>
                                 <div className="row justify-content-center align-items-center">Résumé</div>
                             </div>
